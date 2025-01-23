6f76baa42f8c51702c73805b86f574f0eda6ee0f
Samuel Alfred <samymateru1999@gmail.com>
2025-01-23 12:27:03 +0300

    changin port

--- main.py
+++ main.py
@@ -135,4 +135,4 @@ app.include_router(templates_router, tags=["Templates"])
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=12000)
